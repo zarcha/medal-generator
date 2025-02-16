4e6f10da732202c62f7181f9ef530064e5639b97
Your Name <you@example.com>
2025-02-15 23:53:38 -0600

    update

--- src/components/Instructions.svelte
+++ src/components/Instructions.svelte
@@ -14,17 +14,16 @@
         <hr />
         <h3>Troubleshooting</h3>
         <hr />
-        <ul style="max-width: 500px">
+        <ul>
             <li>
-                Not the expected Medabot?
+                <span class="trouble-title">Not the expected Medabot?</span>
                 <br />
-                These devices are old and cheaply made, the membrane for the medal recognition might need cleaned on the inside but something pressing the buttons a bunch and trying again can fix the issue.
+                These devices are old and cheaply made, the membrane for the medal recognition might need cleaned on the inside but sometimes pressing the buttons a bunch and trying again can fix the issue.
             </li>
-            <br />
             <li>
-                Its a little loose in the watch, is that a problem?
+                <span class="trouble-title">Its a little loose in the watch, is that a problem?</span>
                 <br />
-                The model is made slightly loose for different printers, and im am only 'ok' at modeling but it has not been a problem so far.
+                The model is made slightly loose for different printers, and I am only 'ok' at modeling but it has not been a problem so far.
             </li>
         </ul>
         <p>Still having trouble? Submit an issue to the <a href="https://github.com/zarcha/medal-generator/issues" target="_blank">GitHub</a> project.</p>
@@ -48,4 +47,17 @@
     p {
         text-align: center;
     }
+
+    ul {
+        max-width: 500px;
+
+        li {
+            margin-bottom: 10px;
+        }
+    }
+
+    .trouble-title {
+        font-weight: bolder;
+    }
+
 </style>
