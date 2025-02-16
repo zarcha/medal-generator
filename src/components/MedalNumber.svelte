14b8ede3787482f5f8d06aa2384baf7651d72c61
Your Name <you@example.com>
2025-02-15 22:30:04 -0600

    add advance medal ability and show confirmed medals

--- src/components/MedalNumber.svelte
+++ src/components/MedalNumber.svelte
@@ -1,10 +1,15 @@
 <script>
+    import axios from 'axios';
+    import {onMount} from "svelte";
+
     let { value = $bindable(), generating = $bindable() } = $props();
     let valid = $state(true);
-    let tmpValue = $state()
+    let tmpValue = $state();
+    let medals = $state([]);
+    let matchingMedals = $state([]);
 
     function inputValidation(){
-        valid = tmpValue < 255 && tmpValue > 0;
+        valid = tmpValue > 0 && tmpValue <= 510 && (tmpValue < 255 || tmpValue > 256);
     }
 
     function generate(){
@@ -13,13 +18,43 @@
             value = tmpValue;
         }
     }
+
+    async function getMedalList(){
+        const res = await axios.get('medals.json');
+        medals = res.data;
+    }
+
+    function getMedals(id){
+        return medals.filter((medal) => {
+           if(medal.id === id){
+               return medal;
+           }
+        });
+    }
+
+    onMount(() => {
+        getMedalList();
+    });
+
+    $effect(() => {
+        if(medals.length > 0){
+            matchingMedals = getMedals(tmpValue);
+        }
+    });
 </script>
 
-<div>
+<div style="text-align: center">
     <div class="input-group mb-3">
-        <input type="number" class="form-control {valid ? '' : 'is-invalid'}" bind:value={tmpValue} oninput={inputValidation} placeholder="Medal Number 1-254">
+        <input type="number" class="form-control {valid ? '' : 'is-invalid'}" bind:value={tmpValue} oninput={inputValidation} placeholder="1-254, 257-510">
         <button class="btn btn-warning" onclick={generate}>Generate</button>
     </div>
+    {#if matchingMedals.length > 0}
+    {#each  matchingMedals as medal}
+    <p style="color: green">Name: {medal.name} - Stage: {medal.stage}</p>
+    {/each}
+    {:else if value && valid}
+    <p style="color: orange">No Confirmed Medals</p>
+    {/if}
 </div>
 
 <style>
